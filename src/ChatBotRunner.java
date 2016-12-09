80f3d0e3a7c0c225557aaab68e6fe8cfac30bafd
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2016-12-09 13:27:37 -0500

    initial commit

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -1,4 +1,12 @@
-
-public class ChatBotRunner {
-
+/**
+ * Created by Zak, George and Trevor on 12/8/16
+ * Compter Science Java
+ * Mr. Levin
+ */
+public class ChatBotRunner 
+{
+	public static void main(String[] args)
+	{
+		
+	}
 }
--- src/TrevorBot.java
+++ src/TrevorBot.java
@@ -1,6 +1,6 @@
 /**
- * Created by Zakaria Elkatani on 12/1/16.
- * Description:
+ * Created by Trevor Goldstein on 12/1/16.
+ * Description: 20 questions
  */
 public class TrevorBot {
-}
+
