bfd41829d492a627ca673326ecf92856746f9cee
George Li <gli9465@bths.edu>
2016-12-09 13:42:45 -0500

    Added description to GeorgeBot class

--- src/GeorgeBot.java
+++ src/GeorgeBot.java
@@ -1,6 +1,18 @@
 /**
- * Created by Zakaria Elkatani on 12/1/16.
- * Description:
+ * 
+ * @Author George Li
+ * @Date 12/9/16
+ * @Teacher Mr.Levin
+ * @Period 8
+ *
  */
-public class GeorgeBot {
+/*
+ * a program to play truth or dare with human
+ */
+public class GeorgeBot 
+{
+	public static void main(String[]args)
+	{
+		
+	}
 }
