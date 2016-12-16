649b83b9e578f26b08afb81f1a3b050011074757
George Li <gli9465@bths.edu>
2016-12-16 13:40:54 -0500

    created getResponse to user input

--- src/chatbot/GeorgeBot.java
+++ src/chatbot/GeorgeBot.java
@@ -9,8 +9,24 @@ package chatbot;
 
 public class GeorgeBot 
 {
+	/**
+	 * Get a default greeting
+	 * @return a greeting
+	 */
 	public String getGreeting()
 	{
 		return "Hello, I am here to help you with your love life";
+	}
+	
+	/**
+	 * Gives a response to user statement 
+	 * @param statement 
+	 * 			the user statement
+	 * @return a response based on rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if
 	}
 }
